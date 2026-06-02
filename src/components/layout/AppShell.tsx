759308721118c474467063b2bfbe4a49564a3bbc
Bunstray <rayyanbunasatria06@gmail.com>
2026-06-03 03:10:14 +0700

    IOSUIFix

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -3,8 +3,8 @@ import BottomNav from './BottomNav';
 
 export default function AppShell() {
   return (
-    <div className="relative mx-auto flex min-h-[100dvh] w-full max-w-lg flex-col overflow-x-hidden bg-bg-primary">
-      <main className="flex-1 overflow-y-auto pb-28">
+    <div className="relative mx-auto flex h-[100dvh] w-full max-w-lg flex-col overflow-hidden bg-bg-primary">
+      <main className="flex-1 overflow-y-auto pb-4">
         <Outlet />
       </main>
       <BottomNav />
