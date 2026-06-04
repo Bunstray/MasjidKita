dfa6b48e760ac2e0e4814598c096bc27585f8339
AjiPratama53 <aji.nanindra27@gmail.com>
2026-06-04 10:31:16 +0700

    decompose scanqr to new component

--- src/pages/EKupon.tsx
+++ src/pages/EKupon.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useRef, useState } from "react";
-import { Html5QrcodeScanner, Html5Qrcode } from "html5-qrcode";
 import Header from "@/components/layout/Header";
+import ScanQR from "@/components/layout/ScanQR";
 
 type TabType = 'scan qr' | 'tersimpan';
-
 
 interface CoupounItems {
     id: string;
@@ -16,33 +15,6 @@ interface CoupounItems {
 export default function EKupon() {
   const readerRef = useRef<HTMLDivElement | null>(null);
   const [activeTab, setActiveTab] = useState<TabType>('scan qr');
-  
-  
-  useEffect(() => {
-    if (!readerRef.current) return;
-  
-    const html5QrcodeScanner = new Html5QrcodeScanner(
-      readerRef.current.id,
-      { fps: 10, qrbox: { width: 250, height: 250 } },
-      false
-    );
-  
-    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-  
-    return () => {
-      html5QrcodeScanner.clear().catch((error) => {
-        console.warn("Failed to clear QR scanner", error);
-      });
-    };
-  }, []);
-  
-  function onScanSuccess(decodedText: string, decodedResult: any) {
-    console.log(`Code matched = ${decodedText}`, decodedResult);
-  }
-  
-  function onScanFailure(error: any) {
-    console.warn(`Code scan error = ${error}`);
-  }
 
   return (
     <div className="flex min-h-screen flex-col bg-bg-primary">
@@ -73,15 +45,12 @@ export default function EKupon() {
             </button>
           ))}
         </div>
-          <p className="text-lg text-gray-600">Halaman E-Kupon sedang dalam pengembangan.</p>
-
           {/* QR Code Scanner Placeholder */}
-          {activeTab === 'scan qr' && (
-            <div
-              ref={readerRef}
-              id="reader"
-            ></div>
-          )}
+          {/* {activeTab === 'scan qr' && (
+            <ScanQR />
+          )} */}
+          
+          <p className="text-lg text-gray-600">Halaman E-Kupon sedang dalam pengembangan.</p>
         </div>
     </div> 
 
